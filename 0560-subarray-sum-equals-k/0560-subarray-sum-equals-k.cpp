8ef6fa3278d9b8db6cc94b9497eaa3df16aa4e7a
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-02-14 18:20:12 -0500

    Time: 2563 ms (5.03%), Space: 50.6 MB (11.69%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        multiset<int> a;
+        int sum=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum = sum+nums[i];
+            if(sum==k)
+            {
+                count++;
+            }
+            if(a.find(sum-k)!=a.end())
+            {
+                count+=a.count(*a.find(sum-k));
+            }
+            a.insert(sum);
+        }
+        
+        return count;
+    }
+};
