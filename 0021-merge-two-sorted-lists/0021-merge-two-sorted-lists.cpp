5ed812f97fa3d656bd3a79954b795d5e9e1fcd0b
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-02-15 16:55:05 -0500

    Time: 8 ms (21.81%), Space: 18.5 MB (6.99%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)
+        {
+            return list2;
+        }
+        if(list2==NULL)
+        {
+            return list1;
+        }
+        int a=0;
+        if(list1->val > list2->val)
+        {
+            a= list2->val;
+            list2=list2->next;
+        }
+        else
+        {
+            a=list1->val;
+            list1=list1->next;
+        }
+        ListNode* head = new ListNode(a);
+        ListNode* l1 = list1;
+        ListNode* l2 = list2;
+        ListNode* l3 = head;
+        
+        while(l1!=NULL && l2!=NULL)
+        {
+            if(l1->val > l2->val)
+            {
+                ListNode* temp = new ListNode(l2->val);
+                l3->next = temp;
+                l3=l3->next;
+                l2=l2->next;
+            }
+            else
+            {
+                ListNode* temp = new ListNode(l1->val);
+                l3->next = temp;
+                l3=l3->next;
+                l1=l1->next;
+            }
+        }
+        while(l1!=NULL){
+            ListNode* temp = new ListNode(l1->val);
+                l3->next = temp;
+                l3=l3->next;
+                l1=l1->next;
+            
+        }
+        while(l2!=NULL)
+        {
+            ListNode* temp = new ListNode(l2->val);
+                l3->next = temp;
+                l3=l3->next;
+                l2=l2->next;
+        }
+        return head;
+        
+    }
+};
