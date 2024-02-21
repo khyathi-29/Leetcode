4bb06bf6164c97cfdc3868265c2761201e9e2364
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-02-21 17:18:59 -0500

    Time: 24 ms (82.44%), Space: 30 MB (13.30%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,8 +3,8 @@ public:
     int search(vector<int>& nums, int target) {
         int end = nums.size()-1;
         int start =0;
-        int mid ;
-        while(start<=end)
+        return rsearch(nums,target,start,end);
+       /* while(start<=end)
         {
             mid = start + (end-start)/2;
             if(target == nums[mid])
@@ -19,7 +19,29 @@ public:
             {
                 end = mid-1;
             }
-        }
+        }*/
         return -1;
+    }
+    int rsearch(vector<int>& nums, int target, int start, int end)
+    {
+        if(start > end)
+        {
+            return -1;
+        }
+        int mid = start + ((end-start)/2);
+        cout<<"mid"<<mid;
+        if(target == nums[mid])
+        {
+            return mid;
+        }
+        else if (target > nums[mid])
+        {
+            
+            return rsearch(nums,target,mid+1,end);
+        }
+        else
+        {
+            return rsearch(nums,target,start,mid-1);
+        }
     }
 };
