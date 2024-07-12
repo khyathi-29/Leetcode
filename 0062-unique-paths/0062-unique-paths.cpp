eb378730f9bf627d0e2a0959b2aae692c7844bcd
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-07-12 12:42:46 -0400

    Time: 2 ms (42.75%), Space: 7.5 MB (64.66%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-     int a[m][n];
-     for(int i=0; i<m;i++)
-     {
-         for(int j=0;j<n;j++)
-         {
-             a[i][j]=0;
-         }
-     }
-     for(int i=0; i<m;i++)
-     {
-         a[i][0]=1;
-     }
-     for(int j=0; j<n;j++)
-     {
-         a[0][j]=1;
-     }
-     for(int i=1;i<m;i++)
-     {
-         for(int j=1;j<n;j++)
-         {
-             a[i][j]= a[i][j-1]+a[i-1][j];
-         }
-     }
-     return a[m-1][n-1];
+     int grid[m][n];
+        if(m==1 || n==1) return 1;
+        for(int i=0;i<m; i++)
+        {
+            grid[i][0]=1;
+        }
+        for(int i=0;i<n; i++)
+        {
+            grid[0][i]=1;
+        }
+        for(int i=1 ;i<m;i++)
+        {
+            for(int j = 1 ; j<n;j++)
+            {
+                grid[i][j]=grid[i-1][j]+grid[i][j-1];
+            }
+        }
+         return grid[m-1][n-1];
     }
 };
