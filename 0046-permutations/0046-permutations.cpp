9d65fb7f0ab193257f024b2fc11dafe09112ff32
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-07-28 20:33:57 -0400

    Time: 5 ms (32.14%), Space: 9.2 MB (54.26%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> a;
-        permutation(0,nums,a);
-        return a;
-        
+    void swap(int i, int j, vector<int>& nums)
+    {
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+        return;
     }
-    void permutation(int index , vector<int>& nums,vector<vector<int>>& a) {
-        if(index==nums.size())
+    void permutehelper(int index, vector<int>& nums, vector<vector<int>>& ans)
+    {
+        if(index == nums.size())
         {
-            a.push_back(nums);
+            ans.push_back(nums);
             return;
         }
-        for(int i=index;i<nums.size();i++)
-        {
-            int temp = nums[index];
-            nums[index]=nums[i];
-            nums[i]=temp;
-            permutation(index+1,nums,a);
-            temp = nums[index];
-            nums[index]=nums[i];
-            nums[i]=temp;
-        }
         
+        for(int i = index;i<nums.size();i++)
+        {
+            swap(index,i,nums);
+            permutehelper(index+1,nums,ans);
+            swap(index,i,nums);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        permutehelper(0,nums,ans);
+        return ans;
     }
 };
