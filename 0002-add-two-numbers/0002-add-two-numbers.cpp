1bebfb81343633bc08ec128d9691c4afc98ccc8d
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-08-02 18:29:51 -0400

    Time: 26 ms (34.35%), Space: 77.5 MB (16.45%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -12,83 +12,27 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        int len1 =0;
-        int len2 =0;
-        ListNode* t1= l1;
-        ListNode* t2= l2;
-        vector<int> a;
-        while(t1!=NULL)
+        ListNode* ans = new ListNode(0);
+        ListNode* temp = ans;
+        int carry=0;
+        while(l1!=NULL || l2!=NULL || carry==1)
         {
-            len1++;
-            t1=t1->next;
-        }
-        while(t2!=NULL)
-        {
-            len2++;
-            t2=t2->next;
+            int sum =0;
+            sum+=carry;
+            if(l2!=NULL) {
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            ListNode* temp = new ListNode(sum%10);
+            carry = sum/10;
+            ans->next = temp;
+            ans = ans->next;  
         }
         
-        int len = min(len1,len2);
-        int l=0;
-        if(len1>len2)
-        {
-            t1=l1;
-            t2=l2;
-            l=len1-len2;
-        }
-        else
-        {
-            t1=l2;
-            t2=l1;
-            l=len2-len1;
-        }
-        int k=0;
-        while(len>0)
-        {
-          int i = t1->val + t2->val+k;
-          if(i>9)
-          {
-              k=1;
-          }
-          else
-          {
-              k=0;
-          }
-          a.push_back(i%10);
-          i=0;
-          t1=t1->next;
-          t2=t2->next;
-          len--;
-        }
-        while(l>0)
-        {
-            int i = t1->val + k;
-          if(i>9)
-          {
-              k=1;
-          }
-          else
-          {
-              k=0;
-          }
-          a.push_back(i%10);
-          i=0;
-          t1=t1->next;
-          l--;
-        }
-        if(k==1)
-        {
-            a.push_back(1);
-        }
-        cout<<len1<<len2;
-        ListNode* head= new ListNode(a[0]);
-        ListNode* temp = head;
-        for(int i=1;i<a.size();i++)
-    {
-        ListNode *val = new ListNode(a[i]);
-        temp->next = val;
-        temp = temp->next;
-    }
-       return head; 
+        return temp->next;
     }
 };
