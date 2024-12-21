88b70406551adde61bf5e0e6e38cf989d34e5285
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-12-20 19:58:53 -0500

    Time: 3 ms (64.39%), Space: 10.2 MB (56.29%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,41 +1,56 @@
 class Solution {
 public:
-    void solve(int col,int n, vector<string>& temp, vector<vector<string>>& ans,vector<int>& left, vector<int>& upDiag, vector<int>& downDiag)
-    {
-        if(col==n)
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string>board(n);
+        string s(n, '.');
+        for(int i=0;i<n;i++)
         {
-            ans.push_back(temp);
+            board[i]=s;
         }
-        for(int row=0; row<n; row++)
+        solve(0,board,ans,n);
+        return ans;
+    }
+    void solve(int row,vector<string>& board,vector<vector<string>>& ans, int n)
+    {
+        if(row==n)
         {
-            if(temp[row][col]=='.' && left[row]==0 && upDiag[row+col]==0 && downDiag[n-1+col-row]==0 )
+            ans.push_back(board);
+            return;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(isValid(row,i,board)!=false)
             {
-                 temp[row][col]='Q';
-                 left[row]=1;
-                 upDiag[row+col]=1;
-                 downDiag[n-1-row+col]=1;
-                 solve(col+1,n,temp,ans,left,upDiag,downDiag);
-                temp[row][col]='.';
-                 left[row]=0;
-                 upDiag[row+col]=0;
-                 downDiag[n-1-row+col]=0;
+                board[row][i]='Q';
+                solve(row+1,board,ans,n);
+                board[row][i]='.';
             }
         }
     }
-    vector<vector<string>> solveNQueens(int n) {
-     vector<vector<string>> ans;
-        vector<int> left(n,0);
-        vector<int> upDiag(2*n-1,0);
-        vector<int> downDiag(2*n-1,0);
-        vector<string> temp(n);
-        string s(n,'.');
-     
-        for(int i=0;i<n;i++)
+    bool isValid(int row, int col,vector<string>& board)
+    {
+        for(int i=0;i<board.size();i++)
         {
-            temp[i]=s;
+            if(board[i][col]=='Q') return false;
         }
-        
-        solve(0,n,temp,ans,left,upDiag,downDiag);
-       return ans; 
+        int tempr=row;
+        int tempc = col;
+        while(tempr>-1 && tempc>-1)
+        {
+            if(board[tempr][tempc]=='Q') return false;
+            tempr-= 1;
+            tempc-= 1;
+        }
+        tempr =row;
+        tempc = col;
+        while(tempr>-1 && tempc<board.size())
+        {
+            if(board[tempr][tempc]=='Q') return false;
+            tempr-= 1;
+            tempc+= 1;
+        }
+        return true;
     }
+
 };
