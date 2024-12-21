8ce726d0ce5ddec9ce4d8b78e9409890d58ea0e2
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-12-20 19:54:56 -0500

    Time: 0 ms (100.00%), Space: 10.4 MB (46.37%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    void subset(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans)
+   void subsetwithdup(vector<int>& nums, vector<vector<int>>& ans, vector<int> &temp,int index)
+   {
+    ans.push_back(temp);
+    if(index==nums.size()) return;
+    for(int i = index;i<nums.size();i++)
     {
-        ans.push_back(temp);
-        for(int i = index ; i< nums.size();i++)
-        {
-            if(i>index && nums[i]==nums[i-1]) continue;
-            temp.push_back(nums[i]);
-            subset(i+1,nums,temp,ans);
-            temp.pop_back();
-        }
+        if(i!=index && nums[i]==nums[i-1]) continue;
+        temp.push_back(nums[i]);
+        subsetwithdup(nums,ans,temp,i+1);
+        temp.pop_back();
     }
+   }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         vector<int> temp;
-        sort(nums.begin(),nums.end());
-        subset(0,nums,temp,ans);
+        subsetwithdup(nums,ans,temp,0);
         return ans;
         
     }
