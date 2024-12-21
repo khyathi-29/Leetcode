b10db9035404fd6d48b09f905018d11f791875c9
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-12-20 20:01:06 -0500

    Time: 0 ms (100.00%), Space: 15.2 MB (13.00%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,36 +3,21 @@ public:
     int search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size()-1;
-        
-        while(low<=high)
+        int mid ;
+        while(low <=high)
         {
-            int mid = low + (high-low)/2;
-            if(nums[mid]==target)
+            mid = low + (high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[low]<=nums[mid])
             {
-                return mid;
-            }
-            cout<<mid<<" "<<endl;
-            if(nums[mid]>=nums[low])
-            {
-                if(nums[low]<=target && target<=nums[mid])
-                {
-                    high = mid-1;
-                }
-                else
-                {
-                    low = mid+1;
-                }
+                if(target<nums[mid] && target>=nums[low] ) high = mid-1;
+                 else low = mid+1;
             }
             else
             {
-                if(nums[mid]<=target  && target<=nums[high])
-                {
-                    low = mid + 1;
-                }
-                else
-                {
-                    high = mid -1;
-                }
+                if(target<=nums[high] && target>nums[mid]) low = mid+1;
+                else high = mid -1;
+                
             }
         }
         return -1;
