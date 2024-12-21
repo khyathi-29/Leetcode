0c7e3e3a031e351bad360b8559c9f6d587bda5f8
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-12-20 19:34:42 -0500

    Time: 0 ms (100.00%), Space: 9.8 MB (6.53%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> a(numRows);
-        for(int i=0;i<numRows;i++)
+        for(int i=1 ; i<=numRows ; i++)
         {
-            a[i]=vector<int>(i+1);
-            for(int j=0;j<i;j++)
+            vector<int> b(i,1);
             {
-                a[i][j]=0;
+                a[i-1]=b;
             }
         }
-        for(int i=0;i<numRows;i++)
+        for(int i=1;i<a.size();i++)
         {
-            a[i][0]=1;
-            a[i][a[i].size()-1]=1;
-        }
-        for(int i=1;i<numRows;i++)
-        {
-            for(int j=1;j<i;j++)
+            for(int j = 1 ; j<a[i].size()-1;j++)
             {
                 a[i][j]=a[i-1][j]+a[i-1][j-1];
             }
         }
 
-       return a; 
+        return a;
     }
 };
