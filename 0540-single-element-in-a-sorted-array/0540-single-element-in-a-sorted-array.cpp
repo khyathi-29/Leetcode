301db7164ffce4794224dbfee5ba73889dd0e587
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-12-20 20:00:36 -0500

    Time: 0 ms (100.00%), Space: 26.3 MB (16.32%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1)
-        {
-            return nums[0];
-        }
-        if(nums[0]!=nums[1])
-        {
-            return nums[0];
-        }
-        if(nums[nums.size()-1]!=nums[nums.size()-2])
-        {
-            return nums[nums.size()-1];
-        }
         int low = 1;
-        int high =nums.size()-2;
-       while(low<=high) 
-       {
-           int mid = low + (high-low)/2;
-           if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
-           {
-               return nums[mid];
-           }
-           if(mid%2==0)
-           {
-               if(nums[mid]==nums[mid+1])
-               {
-                   low = mid + 1;
-               }
-               else 
-               {
-                   high = mid-1;
-               }
-           }
-           else
-           {
-                if(nums[mid]==nums[mid-1])
-               {
-                   low = mid + 1;
-               }
-               else 
-               {
-                   high = mid-1;
-               }
-           }
-       }
-      return -1;  
+        int high = nums.size()-2;
+        int mid ;
+        if(nums.size()==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[high]!=nums[high+1]) return nums[high+1];
+        while(low <=high)
+        {
+            int mid = low + ((high-low)/2);
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+            if(mid%2==0)
+            {
+                if(nums[mid]==nums[mid+1]) low = mid+1;
+                else high = mid-1;
+            }
+            else
+            {
+               if(nums[mid]==nums[mid+1]) high = mid-1;
+                else low = mid+1;
+            }
+        }
+        return -1 ;
     }
 };
