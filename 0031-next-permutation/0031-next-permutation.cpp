3672952df61abab1cb8e1e079bfae35e7d6056cf
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2024-12-20 19:35:33 -0500

    Time: 0 ms (100.00%), Space: 15.8 MB (13.05%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-      //1 2 5 4 3 2 1;
-      //5 4 3 2 1
-      int pivot=-1;
-      int n = nums.size();
-      if(n==1)
-      {
-          return;
-      }
-      for(int i=n-2;i>=0;i--)
-      {
-          if(nums[i]<nums[i+1])
-          {
-              pivot = i;
-              //cout<<pivot;
-              break;
-          }
-       } 
-      if(pivot==-1)
-      {
-          sort(nums.begin(),nums.end());
-          return;
-      }
+        int pivot = -1;
 
-       for(int i=n-1;i>-1;i--)
-      {
-          if(nums[i]>nums[pivot])
-          {
-              int temp = nums[pivot];
-              nums[pivot]=nums[i];
-              nums[i]=temp;
-              break;
-          }
-      }
-      for(int i=0;i<n;i++)
-      {
-          cout<<nums[i]<<" ";
-      }
-      cout<<endl;
-      sort(nums.begin()+pivot+1,nums.end());
-      for(int i=0;i<n;i++)
-      {
-          cout<<nums[i]<<" ";
-      }
-      return;
+        for(int i=nums.size()-1;i>0;i--)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                pivot = i-1;
+                break;
+            }
+        }
+        if(pivot == -1)
+        {
+            sort(nums.begin(),nums.end());
+        }
+        else
+        {
+        for(int i= nums.size()-1;i>pivot;i--)
+        {
+            if(nums[i]>nums[pivot])
+            {
+                
+                int b = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot]=b;
+                break;
+                
+            }
+        }
+        sort(nums.begin()+pivot+1,nums.end());
+        }
+        
     }
 };
