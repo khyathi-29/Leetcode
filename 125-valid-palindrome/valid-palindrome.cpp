54d5bdc372af484e16398e30b3d43a5ede54df82
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-01-04 19:05:03 -0500

    Time: 0 ms (100.00%) | Memory: 10 MB (37.67%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-// Using two pointer approach if s[l] is not a between 'a'&&'z' 
+//1. Using two pointer approach if s[l] or s[r] is not a between 'a'&&'z' && 'A'&&'Z' && '0'&&'9' skip
+//2. If either of them is number compare two
     bool isPalindrome(string s) {
         int l =0;
         int r =s.size()-1;
@@ -8,14 +9,14 @@ public:
         {
             if(!((s[l]>='a' && s[l]<='z')||(s[l]>='A' && s[l]<='Z')||(s[l]>='0'&& s[l]<='9')))l++;
             else if(!((s[r]>='a' && s[r]<='z')||(s[r]>='A' && s[r]<='Z')||(s[r]>='0'&& s[r]<='9')))r--;
-            else if((s[l]>='0'&& s[l]<='9') || (s[r]>='0' && s[r]<='9'))
+            /*else if((s[l]>='0'&& s[l]<='9') || (s[r]>='0' && s[r]<='9'))
             {
                 if(s[l]!=s[r]){
                 return false;
                 }
                 l++;
                 r--;
-            }
+            }*/
             else if(tolower(s[l])==tolower(s[r]))
             {
                 l++;
