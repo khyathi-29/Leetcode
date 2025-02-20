c2c35c9a036929f7b7c930d5a9462e247e097ba7
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-02-20 12:34:33 -0500

    Time: 426 ms (51.07%) | Memory: 11.6 MB (35.94%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& board, string word, int& count, int i, int j ){
+    bool check(vector<vector<char>>& board, string word, int count, int i, int j ){
         if(count == 0) return true;
         bool ans;
         if(i>=board.size() || j>=board[i].size() || i<0 || j<0 || word[count-1]!=board[i][j]){
@@ -8,9 +8,7 @@ public:
         }else{
         char temp = board[i][j];
         board[i][j] = '%';
-        count--;
-        ans = check(board, word, count,i+1,j)|| check(board, word, count,i,j+1)||check(board,word,count,i-1,j)||check(board, word, count,i,j-1);
-        count++;
+        ans = check(board, word, count-1,i+1,j)|| check(board, word, count-1,i,j+1)||check(board,word,count-1,i-1,j)||check(board, word, count-1,i,j-1);
         board[i][j] = temp;
         }
         return ans;
