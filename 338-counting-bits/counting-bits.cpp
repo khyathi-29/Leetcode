42b673470039ef81cdca603af914cb2fbcf03816
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-02-20 22:44:39 -0500

    Time: 0 ms (100.00%) | Memory: 10.5 MB (97.28%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-    vector<int> ans(n+1,0);
+    /*vector<int> ans(n+1,0);
     int offset = 1;
     for(int i =1 ;i <n+1;i++){
      if(offset*2==i){
         offset = i;
      }
      ans[i]=1+ans[i-offset];
+    }
+    return ans;*/
+    vector<int> ans(n+1,0);
+    for(int i=1;i<n+1;i++){
+        ans[i]= ans[i>>1]+(i&1);
     }
     return ans;
     }
