e553cf18283c15e5921ce45d788d4c48813f1933
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-03-02 18:31:23 -0500

    Time: 217 ms (5.06%) | Memory: 130.1 MB (5.02%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    //do it using bfs or dfs just start with index if m[i][j]==1 
+    //make all its surrounding index as 0 and start counting as given below
+    // if dfs is used then stack space would be o(m*n) if bfs space would be o(m*n) for dfs
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0 ;
+        int row = grid.size();
+        int col = grid[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(i,j,row,col,grid);
+                }
+            }
+        }
+        return count;
+    }
+        void dfs ( int r,int c,int row,int col, vector<vector<char>>& grid){
+            if(r>=row || c>=col || r < 0 || c < 0) return;
+            else{
+            vector<int> rr = {0,0,+1,-1};
+            vector<int> cc = {-1,+1,0,0};
+            if(grid[r][c] == '1'){
+            grid[r][c]='0';
+            for(int i=0;i<4;i++){
+                dfs(r+rr[i],c+cc[i],row,col,grid);
+            }
+            }
+            }
+        }
+
+        
+};
