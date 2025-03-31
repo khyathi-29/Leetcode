62e33ae4fa8965a34c3fd9305700374809d3d68d
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-03-31 18:09:51 -0400

    Time: 0 ms (100.00%) | Memory: 23.6 MB (93.06%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
         int start = intervals[0][0];
         int end = intervals[0][1];
-        vector<vector<int>> ans;
-        for(int i=1;i<intervals.size();i++)
-        {
-            if(intervals[i][0]<=end)
-            {
-                start = min(intervals[i][0],start);
-                end = max(intervals[i][1],end);
+        for(int i=1;i<intervals.size();i++){
+            if(end>=intervals[i][0]){
+                end = max(end,intervals[i][1]);
             }
             else{
                 ans.push_back({start,end});
                 start = intervals[i][0];
                 end = intervals[i][1];
-
             }
         }
-        ans.push_back({start,end});
-        return ans;
+         ans.push_back({start,end});
+         return ans;
     }
 };
