de54aaa30237afd38b5c090a2a212a1991849972
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-04-03 14:40:32 -0400

    Time: 7 ms (21.93%) | Memory: 22.8 MB (86.09%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        vector<vector<int>> ans;
-        int n = firstList.size();
-        int m = secondList.size();
         int i=0;
         int j=0;
-        while(i<n && j<m){
-            if(firstList[i][0]<=secondList[j][1] && secondList[j][0]<=firstList[i][1]){
-                int start = max(firstList[i][0],secondList[j][0]);
-                int end = min(firstList[i][1],secondList[j][1]);
-                ans.push_back({start,end});
-                if(firstList[i][1]<=secondList[j][1]) i++;
-                else j++;
+        vector<vector<int>> ans;
+        while(i<firstList.size() && j<secondList.size()){
+            int t1 = max(firstList[i][0],secondList[j][0]);
+            int t2 = min(firstList[i][1],secondList[j][1]);
+            if(t1<=t2){//for intersection to exist this should be valid
+                ans.push_back({t1,t2});
             }
-            else{
-                if(firstList[i][0]<secondList[j][0]) i++;
-                else j++;
-            }
+            if(firstList[i][1] < secondList[j][1]) i++;//traversing next elements
+            else j++;
         }
         return ans;
         
