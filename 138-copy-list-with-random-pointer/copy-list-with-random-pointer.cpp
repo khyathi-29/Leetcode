c58d5b411324d85411f45827c7637e13f52a8cf2
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-04-03 19:14:29 -0400

    Time: 7 ms (53.40%) | Memory: 15.1 MB (72.59%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,26 +17,33 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*> mp; // maps old list to its corresonding new list
-        if(head ==NULL ) return NULL;
-        Node* oldhead = head;
-        Node* ans = new Node(-1);
-        Node* temp = ans;
-        while(head!=NULL){
-            Node* t = new Node(head->val);
-            temp->next = t;
-            temp = temp->next;
-            mp[head]=t;
-            head = head->next;
+        Node* h = head;
+        if(h==NULL) return h;
+        // making new nodes and placing them next to the old nodes;
+        while(h!=NULL){
+         Node* temp = new Node(h->val);
+         temp->next = h->next;
+         h->next = temp;
+         h = h->next->next;
         }
-        Node* track = ans->next;
-        while(oldhead!=NULL){
-            Node* rand = mp[oldhead->random];
-            track->random = rand;
-            track = track->next;
-            oldhead=oldhead->next;
+        h=head;
+        Node* ans = head->next;
+        // now mapping random pointers
+        while(h!=NULL){
+            Node* rand = h->random;
+            if(rand)h->next->random = rand->next;// since new pointer would be next to old pointer
+            else h->next->random = NULL;
+            h=h->next->next; 
+
         }
-        return ans->next;
-        
+        h=head;
+        // Now separating new and old pointers
+        while(h!=NULL){
+            Node* newNode = h->next;
+            h->next = newNode->next; // Restore original list's next pointer
+            newNode->next = (h->next != NULL) ? h->next->next : NULL; // Set next pointer for copied list
+            h = h->next; // Move to the next original node
+        }
+        return ans;
     }
 };
