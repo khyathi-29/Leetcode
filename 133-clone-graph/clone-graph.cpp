7ed46bb331aac75a9126aa2657a3bdada705a20b
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-04-05 14:32:29 -0400

    Time: 3 ms (68.14%) | Memory: 12 MB (77.90%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -28,8 +28,6 @@ public:
      Node* start =  new Node(node->val);
      mp[node]=start;
      q.push(node);
-     vector<bool> visited(101,false);
-     visited[node->val]=true;
      while(!q.empty()){
         Node* current = q.front();
         q.pop();
@@ -37,16 +35,15 @@ public:
             if(mp.find(it)==mp.end()){
                 Node* temp = new Node(it->val);
                 mp[it]=temp;
+                q.push(it);
+                // instead of bool we can track elements visited or not visited from map[node] existence
             }
             mp[current]->neighbors.push_back(mp[it]);
-            if(visited[it->val]==false){
-            visited[it->val]=true;
-            // we need to make it true as soon as adding it to queue so that we don't add to queue againa and again it again and again
-             q.push(it);
+    
             }
             
         }
-     } 
+      
      return start; 
     }
 };
