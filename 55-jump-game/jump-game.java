995e00e06dde9b14a8d0249facd687eb69f0a134
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-05-19 15:43:35 -0400

    Time: 2 ms (82.39%) | Memory: 46.1 MB (13.89%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int target = nums.length-1;
+        for(int i = nums.length-2;i>-1;i--)
+        {
+            if(nums[i]+ i >= target) target = i;
+        }
+        if(target==0) return true;
+        else return false;
+    }
+}
