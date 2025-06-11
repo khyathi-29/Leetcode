c6bddd89a5599f6702136c27c228b7535ab58f36
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-06-11 18:44:24 -0400

    Time: 31 ms (69.85%) | Memory: 58.4 MB (73.56%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -2,27 +2,17 @@ class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[0]-b[0]));
         List<List<Integer>> ans = new ArrayList<>();
-        pq.add(new int[]{nums1[0]+nums2[0],0,0});
-        Set<String> s = new HashSet<>();
-        String key = 0+"#"+0;
-        s.add(key);
+        for(int i=0;i<Math.min(nums1.length,k);i++)
+        {
+           pq.add(new int[]{nums1[i]+nums2[0],i,0});
+        }
         while(!pq.isEmpty() && ans.size()<k)
         {
             int[] temp = pq.poll();
             int i = temp[1];
             int j = temp[2];
             ans.add(Arrays.asList(nums1[i],nums2[j]));
-            String key1= (i+1)+"#"+j;
-            String key2= i+"#"+(j+1);
-            if(i+1<nums1.length && s.contains(key1)==false){
-                pq.add(new int[]{nums1[i+1]+nums2[j],i+1,j});
-                s.add(key1);
-            } 
-            if(j+1<nums2.length && s.contains(key2)==false) {
-                pq.add(new int[]{nums1[i]+nums2[j+1],i,j+1});
-                s.add(key2);
-            
-            }
+            if(j+1 < nums2.length) pq.add(new int[]{nums1[i]+nums2[j+1],i,j+1});
         }
         return ans;
     }
