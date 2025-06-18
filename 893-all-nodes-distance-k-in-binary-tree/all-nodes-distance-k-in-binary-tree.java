77e388afa057b49750630522d2a7f33b69ea258a
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-06-18 18:30:48 -0400

    Time: 11 ms (97.55%) | Memory: 42.8 MB (52.65%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Set<TreeNode> vis = new HashSet<>();
+        Map<TreeNode,TreeNode> mp = new HashMap<>();
+        getParent(root,mp);
+        List<Integer> ans = new ArrayList<>();
+        Helper(target, k,mp,ans,vis);
+        return ans;
+     
+    }
+    void  Helper(TreeNode root, int target,Map<TreeNode,TreeNode> mp,List<Integer> ans,Set<TreeNode> vis){
+        if(root==null) return ;
+        if(target==0){
+            ans.add(root.val);
+            return;
+        }
+        vis.add(root);
+        if(root.left!=null && vis.contains(root.left)==false) {
+            Helper(root.left,target-1,mp,ans,vis);
+        }
+         if(root.right!=null && vis.contains(root.right)==false){
+           Helper(root.right,target-1,mp,ans,vis);
+         } 
+         if(mp.containsKey(root)==true && vis.contains(mp.get(root))==false)
+         {
+            Helper(mp.get(root),target-1,mp,ans,vis);
+         }
+    }
+    void getParent(TreeNode root, Map<TreeNode,TreeNode> mp)
+    {
+        if(root==null) return;
+        if(root.left!=null){
+            mp.put(root.left,root);
+            getParent(root.left,mp);
+        }
+        if(root.right!=null){
+            mp.put(root.right,root);
+            getParent(root.right,mp);
+        }
+    }
+}
