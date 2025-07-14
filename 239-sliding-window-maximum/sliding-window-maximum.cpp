b9274b97754b78ee6d1338a289e66357a9795882
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-07-14 19:22:16 -0400

    Time: 27 ms (46.54%) | Memory: 139.2 MB (60.30%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> de;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            while(!de.empty() && nums[de.back()] <= nums[i])
+            {
+                de.pop_back();
+            }
+            // above loop removes values which are less than the current index
+            // and makes sure loop is in descending order;
+            de.push_back(i);
+            
+            if(de.front()< i-k+1)
+            {
+                de.pop_front();
+            }
+            if(i>=k-1)
+            {
+                int a = nums[de.front()];
+                ans.push_back(a);
+            }
+        }
+        return ans;
+        
+    }
+};
