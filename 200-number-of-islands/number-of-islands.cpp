6dcbde3509b66e04ae83a08f0f49a99a571f6c04
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-07-13 23:27:48 -0400

    Time: 29 ms (45.57%) | Memory: 16.1 MB (89.77%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-    //do it using bfs or dfs just start with index if m[i][j]==1 
-    //make all its surrounding index as 0 and start counting as given below
-    // if dfs is used then stack space would be o(m*n) if bfs space would be o(m*n) for dfs
-    int count = 0;
-    for(int i=0;i<grid.size();i++){
-        for(int j=0;j<grid[0].size();j++){
-              if(grid[i][j]=='1'){
-                count++;
-                dfs(grid,i,j);
-              }
-        }
-    }
-       return count; 
-    }
-    void dfs(vector<vector<char>>& grid,int i,int j){
-        grid[i][j]='0';
-        vector<int> r = {-1,1,0,0};
-        vector<int> c = {0,0,-1,1};
-        for(int k=0;k<4;k++){
-            int row = i + r[k];
-            int col = j + c[k];
-            if(row>-1 && row<grid.size() && col>-1 && col<grid[0].size() && grid[row][col]=='1'){
-             dfs(grid,row,col);
+        int count = 0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    count++;
+                    dfs(grid,i,j);
+                }
             }
         }
+        return count;
+        
+    }
+    void dfs(vector<vector<char>>& grid, int i, int j )
+    {
+        grid[i][j]='0';
+        int r[4] = {+1,-1,0,0};
+        int c[4] = {0,0,+1,-1};
+        for(int k =0;k<4;k++)
+        {
+            int r1 = i+r[k];
+            int c1 = j+c[k];
+            if(r1>-1 && r1<grid.size() && c1>-1 && c1<grid[0].size())
+            {
+                if(grid[r1][c1]=='1') dfs(grid,r1,c1);
+            }
+        }
+        return;
     }
 };
