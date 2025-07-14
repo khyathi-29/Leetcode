6c0342f603b830085c3d8a511cb541c6eb8e51db
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-07-14 18:37:04 -0400

    Time: 79 ms (23.07%) | Memory: 125 MB (73.92%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,52 +1,31 @@
 class MedianFinder {
-priority_queue<int> maxHeap;// to store minimum elements
-priority_queue<int,vector<int>,greater<int>> minHeap;//to store maximum elements
+private:
+     priority_queue<int> minHeap;// return min value to store max values
+     priority_queue<int,vector<int>,greater<int>> maxHeap; // 
+     // return max value to store min value;
 public:
     MedianFinder() {
-        
     }
     
     void addNum(int num) {
-        if(maxHeap.empty()){
-            maxHeap.push(num);
+        minHeap.push(num);
+        maxHeap.push(minHeap.top());
+        minHeap.pop();
+        if(maxHeap.size()>1+minHeap.size())
+        {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
         }
-        else if (minHeap.empty()){
-            if(maxHeap.top()> num){
-                int k = maxHeap.top();
-                maxHeap.pop();
-                maxHeap.push(num);
-                minHeap.push(k);
-            } else minHeap.push(num);
-        }
-        else if(maxHeap.size()==minHeap.size()){
-            if(num > minHeap.top()){
-                int k = minHeap.top();
-                minHeap.pop();
-                maxHeap.push(k);
-                minHeap.push(num);
-            }
-            else maxHeap.push(num);
-        }
-        else if (maxHeap.size()> minHeap.size()){
-              if(num < maxHeap.top()){
-                int k = maxHeap.top();
-                maxHeap.pop();
-                maxHeap.push(num);
-                minHeap.push(k);
-              }
-              else minHeap.push(num);
-
-        }
-        
+        return;
     }
     
     double findMedian() {
-        if (maxHeap.size() > minHeap.size()) {
-            return maxHeap.top();
+        if(maxHeap.size()==0) return 0.0;
+        else if(minHeap.size()==maxHeap.size())
+        {
+            return (double)(minHeap.top()+maxHeap.top())/2.0;
         }
-        
-        return (maxHeap.top() + minHeap.top()) / 2.0;
-        
+        else return (double)maxHeap.top();
     }
 };
 
