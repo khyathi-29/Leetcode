be92e9cd686b2f0abbf456dfbb20480bbe82e646
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-07-15 11:50:07 -0400

    Time: 38 ms (61.89%) | Memory: 105.8 MB (60.55%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -8,9 +8,14 @@ public:
         int ans  = 1;
         for(right =1;right<nums.size();right++){
              numIncrease += 1L*(nums[right]-nums[right-1])*(right-left);
+             //since we calculated till before what increase is needed
+             // we just need to calculate increase with before element and multiply
+             // with the width since now we assume the before elements all values are updated to its value;
              while(numIncrease > k)
              {
                 numIncrease -= 1L*nums[right]-nums[left];
+                // since we are removing left element we need to subtract increase 
+                // it caused
                 left++;
              }
              ans = max(ans,right+1-left);
