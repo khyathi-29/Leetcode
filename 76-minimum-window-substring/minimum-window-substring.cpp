6308f5694f9b9a12474a9a439b5ff7a5194a70fd
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-07-15 18:06:27 -0400

    Time: 585 ms (5.02%) | Memory: 11.6 MB (44.28%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int> f1;
-        unordered_map<char,int> f2;
-        if(s.size()<t.size()) return "";
-        for( char c: t) f1[c]++;
-        int match = f1.size();
-        int start = 0;
-        int end = -1;
-        int len = INT_MAX;
-        int left =0;
-        int right =0;
-        while(right<s.size()){
-            char c = s[right];
-            f2[c]++;
-            if(f1.find(c)!=f1.end() && f2[c]==f1[c]) match--;
-            while(match==0){
-                if(right+1-left<len){
-                    start =left;
-                    end = right;
-                    len = right+1-left;
+        unordered_map<char,int> sm;
+        unordered_map<char,int> tm;
+        for(char c : t){
+            tm[c]++;
+        }
+        int match = tm.size();
+        int minLen = s.size()+1;
+        int left = 0;
+        int right = 0;
+        int start =-1;
+        while(right<s.size())
+        {
+            char ch = s[right];
+            sm[ch]++;
+            if(sm[ch]==tm[ch]) match--;
+            while(match==0 && left<=right)
+            {
+                if(minLen>right+1-left) {
+                    start = left;
+                    minLen=right+1-left;
+                     cout<<left<<" right "<<right<<"minLength "<<minLen<<endl;
                 }
-                f2[s[left]]--;
-                if(f1.find(s[left])!=f1.end() && f1[s[left]]>f2[s[left]]) match++;
-                left = left+1;
+                char temp = s[left];
+                sm[temp]--;
+                left++;
+                if(tm.find(temp) != tm.end() && sm[temp]<tm[temp]) match++;
             }
             right++;
-            
         }
-        if(end==-1) return "";
-        return s.substr(start,len);
+
+        if(start!=-1) return s.substr(start,minLen);
+        else return "";
         
     }
 };
