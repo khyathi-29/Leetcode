da08bd1437788c3d3cfd65a957a9dd0cd096d53c
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-07-30 13:49:17 -0400

    Time: 1 ms (47.67%) | Memory: 9.7 MB (95.04%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -7,13 +7,15 @@ public:
         return ans;
     }
     void Helper(int index,vector<int>& nums,vector<vector<int>>& ans,vector<int>& temp){
-        if(index==nums.size()){
+        if(index==nums.size())
+        {
             ans.push_back(temp);
             return;
         }
-        Helper(index+1,nums,ans,temp);
+        // 2 cases if you consider this number 
         temp.push_back(nums[index]);
         Helper(index+1,nums,ans,temp);
         temp.pop_back();
+        Helper(index+1,nums,ans,temp);
     }
 };
