c5c4a98173779593849b5f13081252ec57a15a74
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-08-06 14:01:41 -0400

    Time: 1 ms (87.13%) | Memory: 19.2 MB (74.77%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
 // if there is a cycle in a graph ==> its not possible 
+// detecting a cycle in directed graph using dfs and bfs 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> inDegree(numCourses,0);
         vector<vector<int>> adj(numCourses);
         for(int i=0;i<prerequisites.size();i++){
-            inDegree[prerequisites[i][1]]++;
-            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            inDegree[prerequisites[i][0]]++;
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
         queue<int> q;
         for(int i=0;i<numCourses;i++){
