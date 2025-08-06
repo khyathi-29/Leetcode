13179c107ef0751c4758345278dc419850e41b10
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-08-06 16:31:05 -0400

    Time: 51 ms (61.60%) | Memory: 13.1 MB (84.78%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1,INT_MAX/2);
+        dp[0]=0;
+        int m = sqrt(n);
+        for(int i =1 ; i<=m ;i++)
+        {
+              int sqr = i*i;
+            for(int j = sqr ; j<=n; j++)
+            {
+                dp[j] = min(dp[j],dp[j-sqr]+1);
+            }
+        }
+
+         return dp[n];
+    }
+};
