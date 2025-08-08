dadc9f668b38871879639529d1c89f78519c4c89
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-08-08 12:57:33 -0400

    Time: 0 ms (100.00%) | Memory: 22.1 MB (80.17%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> left(n,1);
-        vector<int> right(n,1);
-        for(int i =1;i<n;i++)
-        {
-            if(ratings[i]>ratings[i-1]) left[i]=left[i-1]+1;
-
-        }
-        for(int i =n-2;i>-1;i--)
-        {
-            if(ratings[i]>ratings[i+1]) right[i]=right[i+1]+1;
-            
+       if (ratings.size() < 2) {
+            return ratings.size(); // If only 1 child, give 1 candy
         }
 
-        int ans =0;
-        for(int i=0;i<n;i++) ans+=max(right[i],left[i]);
-        return ans;
+        int count = 1;           // total candies so far (first child gets 1)
+        int upSlopeLength = 0;   // length of last upward slope segment
+        int upSlope = 0;         // counter for current upward slope
+        int downSlope = 0;       // counter for current downward slope
+
+        for (int i = 1; i < ratings.size(); i++) {
+            int previous = ratings[i - 1];
+            int me = ratings[i];
+
+            // Case 1: Equal ratings
+            if (me == previous) {
+                upSlope = 0;
+                downSlope = 0;
+                upSlopeLength = 0;
+                count += 1; // each flat kid gets exactly 1 candy
+            }
+            // Case 2: Upward slope (current rating higher than previous)
+            else if (me > previous) {
+                upSlope = (upSlope == 0 ? 1 : upSlope) + 1; // extend upSlope
+                downSlope = 0;
+                count += upSlope; // give 1 more candy than previous child
+                upSlopeLength = upSlope; // mark the peak length
+            }
+            // Case 3: Downward slope (current rating lower than previous)
+            else {
+                upSlope = 0;
+                downSlope += 1;
+                // Peak adjustment: if downhill is longer than uphill,
+                // the peak child needs 1 extra candy
+                int slopeDiff = (downSlope + 1 - upSlopeLength) > 0 ? 1 : 0;
+                count += downSlope + slopeDiff;
+            }
+        }
+
+        return count;
     }
 };
