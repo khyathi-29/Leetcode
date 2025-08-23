f2c9648f49b24d8219e678ac2b0a2d1900294bff
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-08-23 19:26:06 -0400

    Time: 0 ms (100.00%) | Memory: 10.4 MB (72.63%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
         vector<string> temp;
-        vector<string> ans ;
-        if(s.size()>12) return ans;
-        Helper(0,s,temp,ans);
+        int index = 0;
+        int nums = 0;
+        Helper(0,ans,temp,s);
         return ans;
     }
-    void Helper(int index ,string s, vector<string>& temp,vector<string>& ans)
-    {
-        if(index == s.size() && temp.size()==4)
+     void Helper(int index, vector<string>& ans, vector<string>& temp,string s)
         {
-            ans.push_back(temp[0]+"."+temp[1]+"."+temp[2]+"."+temp[3]);
-            return;
+            if(temp.size()==4 && index==s.size()) {
+                string temp1 = temp[0]+"."+temp[1]+"."+temp[2]+"."+temp[3];
+                ans.push_back(temp1);
+                return;
+            }
+            if(index==s.size() || temp.size()==4) return;
+            int nums = 0;
+            for(int i= index; i< min(index+3,(int)s.size());i++)
+            {
+                    int val = s[i]-'0';
+                    nums = nums*10 + val;
+                    if(nums>255) break;
+                    string ss = s.substr(index, i-index+1);
+                    if(ss.size()>1 && ss[0]=='0') break;
+                    temp.push_back(ss);
+                    Helper(i+1,ans,temp,s);
+                    temp.pop_back();
+            }
+            
         }
-        if(index==s.size()) return;
-        if(temp.size()>4) return;
-        int val = 0;
-        string num = "";
-        for(int i = index ;i< min(index+3,(int)s.size());i++)
-        {
-           val = val*10+ s[i]-'0';
-           if(val>255 || (s[index]=='0') && i!=index) break;
-           //removes invalid numbers greater than 255 and index with leading zeroes
-           temp.push_back(s.substr(index, i+1-index));
-           Helper(i+1,s,temp,ans);
-           temp.pop_back();
-        }
-    }
 };
