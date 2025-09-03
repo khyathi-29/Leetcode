fb4f8566ae56a149c1d03b35f5c99ae0bf9da76e
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-09-03 01:39:40 -0400

    Time: 0 ms (100.00%) | Memory: 9.2 MB (77.47%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -8,13 +8,15 @@ public:
         if(s!=n+m) return false;
 
         vector<vector<bool>> dp (n+1,vector<bool>(m+1,false)) ;
-        dp[0][0]=true;
+         dp[0][0]=true;
         for(int i=0;i<n+1;i++){
             for(int j=0;j<m+1;j++){
                 int k = i+j;
-                if(i>0 && s1[i-1]==s3[k-1]) dp[i][j]=dp[i-1][j];
-                if(j>0 && s2[j-1]==s3[k-1] && dp[i][j]==false) dp[i][j]= dp[i][j-1];
+                
+                if(i>0 && s1[i-1]==s3[k-1]) dp[i][j]=dp[i-1][j] || dp[i][j];
+                if(j>0 && s2[j-1]==s3[k-1]) dp[i][j]= dp[i][j-1] || dp[i][j];
             }
+            
         }
         return dp[n][m];
 
