6f94f83f3dcb36092ecfdd087134671c5acb3862
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-09-03 00:02:01 -0400

    Time: 44 ms (62.53%) | Memory: 57.4 MB (63.56%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -3,11 +3,27 @@ public:
     vector<vector<string>> partition(string s) {
       vector<vector<string>> ans;
       vector<string> temp;
-      Helper(0,s,ans,temp);  
+      int n = s.size();
+      // pre compute if string [i--j] is palindrome or not
+      vector<vector<bool>>isPalindrome(n,vector<bool>(n,true));
+
+      // to check
+      //Build palindrome lookup table using dynamic programming
+        // For any substring, it's a palindrome if:
+        // 1. First and last characters match AND
+        // 2. The substring between them is also a palindrome
+      for(int i= n-1;i>-1;i--)
+      {
+        for(int j=i+1;j<n;j++)
+        {
+            isPalindrome[i][j]= (s[i]==s[j])&& isPalindrome[i+1][j-1];
+        }
+      }
+      Helper(0,isPalindrome, s,ans,temp);  
       return ans;
         
     }
-    void Helper(int index, string s, vector<vector<string>>& ans, vector<string>& temp)
+    void Helper(int index,vector<vector<bool>>& isPalindrome, string s, vector<vector<string>>& ans, vector<string>& temp)
     {
        // cout<<"called"<<endl;
         if(index==s.size()){
@@ -17,22 +33,13 @@ public:
         for(int i = index ; i < s.size();i++){
             //cout<<"insideloop"<<endl;
             string a = s.substr(index,i-index+1);
-            if(isPalindrome(a)==true){
+            if(isPalindrome[index][i]==true){
                 temp.push_back(a);
-                Helper(i+1,s,ans,temp);
+                Helper(i+1,isPalindrome,s,ans,temp);
                 temp.pop_back();
             }
             
         }
     } 
-    bool isPalindrome(string s){
-        int start = 0;
-        int end = s.size()-1;
-        while(start<=end){
-        if(s[start]!=s[end]) return false;
-           start++;
-           end--;
-        }
-        return true;
-    }
+    
 };
