f8e934f16af21b5ae620f96099f3d31c31550172
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-09-05 02:46:13 -0400

    Time: 61 ms (5.04%) | Memory: 49.9 MB (5.50%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,21 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> mp = new HashMap<>();
-       for(String s : strs){
-           int[] freq = new int[26];
-           for(char c : s.toCharArray()){
-                 freq[c-'a']++;
-           }
-           StringBuilder sb = new StringBuilder();
-           for(int i : freq){
-              sb.append("#");
-              sb.append(i);
-           }
-           if(mp.containsKey(sb.toString())==false){
-            mp.put(sb.toString(),new ArrayList<String>());
-           }
-           mp.get(sb.toString()).add(s);
-       } 
-       return new ArrayList(mp.values());
+        Map<String, List<String>> mp = new HashMap<>();
+        List<List<String>> ans = new ArrayList<>();
+        for( String s : strs)
+        {
+             int[] freq = new int[26];
+            for(Character ch : s.toCharArray())
+            {
+                freq[ch-'a']++;
+            }
+            StringBuilder sb= new StringBuilder("");
+            for(int i=0;i<26;i++)
+            {
+               sb = sb.append( i+","+freq[i]+",");
+            }
+            if(mp.get(sb.toString())==null){
+                 mp.put(sb.toString(),new ArrayList<>());
+            }
+            mp.get(sb.toString()).add(s);
+
+        }
+        
+        return new ArrayList<>(mp.values());
     }
 }
