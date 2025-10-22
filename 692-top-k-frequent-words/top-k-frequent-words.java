6fa0fcabdeb372a5c7ec1d069c9ffdc8c19f58b4
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-10-21 22:48:06 -0400

    Time: 10 ms (10.34%) | Memory: 44.7 MB (88.90%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         List<String> ans = new ArrayList<>();
-        Map<String,Integer> mp = new TreeMap<>();
+        Map<String, Integer> mp = new TreeMap<>();
         int n = words.length;
-        for(String s : words)
-        {
-            mp.put(s,mp.getOrDefault(s,0)+1);
+        for (String s : words) {
+            mp.put(s, mp.getOrDefault(s, 0) + 1);
         }
-        List<String>[] temp= new ArrayList[n];
-        for(Map.Entry<String, Integer> entry : mp.entrySet())
-        {
+        List<String>[] temp = new ArrayList[n];
+        for (Map.Entry<String, Integer> entry : mp.entrySet()) {
             int kk = entry.getValue();
             String s = entry.getKey();
-            if(temp[kk]==null) temp[kk]= new ArrayList<>();
+            if (temp[kk] == null)
+                temp[kk] = new ArrayList<>();
             temp[kk].add(s);
         }
-        for(int i = temp.length-1;i>-1;i--)
-        {
-            if(temp[i]!=null)
-            {
-                for( String s : temp[i])
-                {
+        for (int i = temp.length - 1; i > -1; i--) {
+            if (temp[i] != null) {
+                for (String s : temp[i]) {
                     ans.add(s);
                     k--;
-                    if(k==0) return ans;
+                    if (k == 0)
+                        return ans;
                 }
             }
         }
