5b7ce2f0fdee32b9fae2dbd76d533d8b2212d51c
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-10-25 00:58:50 -0400

    Time: 1 ms (96.92%) | Memory: 40.8 MB (94.40%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,16 +1,15 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int low = 1;
-        int high = n;
-        while(low<=high)
+        int l = 0;
+        int h = n;
+        while(l<=h)
         {
-            int mid = low + (high-low)/2;
-            long val = (long) mid*(mid+1)/2;
-            if(val==n) return mid;
-            else if(val<n) low = mid+1;
-            else high = mid-1;
-            
+            int mid = l + (h-l)/2;
+            long sum =(long)mid*(mid+1)/2;
+            if(sum==n) return mid;
+            else if(sum<n)l = mid+1;
+            else h = mid-1;
         }
-        return low-1;
+        return h;
     }
 }
