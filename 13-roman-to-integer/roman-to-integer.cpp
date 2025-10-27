381cc026c5183b46957135e73030c9dbec7f6855
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-10-27 19:54:52 -0400

    Time: 0 ms (100.00%) | Memory: 12.3 MB (68.05%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-//1. If traversing from last element if current < prev then subtract current from total
-
-    int romanToInt(string s) {
-     unordered_map<char,int> umap;
-     umap['I']=1;
-     umap['V']=5;
-     umap['X']=10;
-     umap['L']=50;
-     umap['C']=100;
-     umap['D']=500;
-     umap['M']=1000;
-     int l = s.size();
-     int total=umap[s[l-1]];
-     int prev = umap[s[l-1]];
-     int current ;
-     for(int i= l-2;i>-1;i--)
-     {
-        int current = umap[s[i]];
-        if(current<prev)
+    int romanToInt(string ss) {
+        int n = ss.length()-1;
+        if(n<0) return 0;
+        unordered_map<char,int> s;
+        s['I'] = 1;
+        s['V'] = 5;
+        s['X'] = 10;
+        s['L'] = 50;
+        s['C'] = 100;
+        s['D'] = 500;
+        s['M'] = 1000;
+        int ans = s[ss[n]];
+        int prev = ans;
+        int current;
+        for(int i = n-1; i>=0 ;i--)
         {
-            total-=current;
+            int current = s[ss[i]];
+            if(current<prev) ans -=current;
+            else ans+=current;
+            prev = current;
         }
-        else total+=current;
-        prev = current;
-     }
-     return total;
+        return ans;
     }
 };
