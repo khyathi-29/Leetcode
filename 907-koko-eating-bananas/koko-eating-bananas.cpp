93e5579658407b02f66c68cbb11c4125c6ba3974
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-10-29 00:14:24 -0400

    Time: 8 ms (58.62%) | Memory: 22.9 MB (44.79%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-     int minspeed = 1; // minimum no. of bananas in a pile 
-     int maxspeed = piles[0];
-     for(int i : piles){
-        if(i > maxspeed) maxspeed = i; // maximum no of bananas monkey can eat in minimum hours 
-     }
-     int mid =0;
-     int ans = 0;
-     while(minspeed<=maxspeed){
-        mid = minspeed + (maxspeed-minspeed)/2 ;
-        long long  k =  0;
-        for(int i=0;i<piles.size();i++){
-            k += (long long) (piles[i] + mid - 1)/ mid;//ceil of number 
+        int low = 1;
+        int high = 1;
+        for(int i : piles){
+            if(high<i) high = i;
         }
-        if (k > h) minspeed = mid +1;
-        else{
-            ans = mid;
-            maxspeed= mid -1;
+        int ans = high;
+        while(low<=high)
+        {
+            int mid = low + (high -low)/2;
+            long hours = 0;
+            for(int i : piles)
+            {
+                hours += (long)i/mid;
+                if(i%mid!=0) hours++;
+            }
+            if(hours>h){
+                low = mid+1;
+            }
+            else {
+                ans = min(mid,ans);
+                high= mid-1;
+            }
+            
         }
-        
-     }
-     return ans;
+        return low;
     }
 };
