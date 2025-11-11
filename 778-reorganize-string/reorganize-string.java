507ccba9035580ddb035fe967e971adff9742215
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-11-10 22:47:37 -0500

    Time: 1 ms (90.60%) | Memory: 42.9 MB (14.26%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,52 +1,53 @@
 class Solution {
-    public String reorganizeString(String s) {
+    String reorganizeString(String s) {
        int[] freq = new int[26];
-       int half = (s.length()+1)/2;
-       int maxfreq = 0;
        for(char c : s.toCharArray())
        {
-         freq[c-'a']++;
-         maxfreq = Math.max(freq[c-'a'],maxfreq);
+        freq[c-'a']++;
        }
-       List<int[]> fr = new ArrayList<>();
-       for(int i =0;i<freq.length;i++)
+       int maxfreq = 0;
+       int maxchar = 0;
+       for(int i=0;i<26;i++)
        {
-        if(freq[i]>0)
-        {
-            int[] temp = new int[2];
-            temp[0]=freq[i];
-            temp[1]=i;
-            fr.add(temp);
+        if(freq[i]>maxfreq){
+            maxfreq = freq[i];
+            maxchar = i;
         }
        }
-       fr.sort((a,b)->Integer.compare(b[0],a[0]));
-       if(maxfreq>half) return "";
-       int i =0;
-       int j =0;
+       
        StringBuilder sb = new StringBuilder(" ".repeat(s.length()));
-       while(i<s.length() && j<fr.size())
+       if(maxfreq>(1+s.length())/2) return "";
+       int i =0;
+       while(i<s.length() && maxfreq>0)
+       {    
+            sb.setCharAt(i,(char)('a'+maxchar));
+            maxfreq--;
+            i=i+2;
+
+       }
+       freq[maxchar]=0;
+       int j =0;
+       while(i<s.length() && j<26)
        {
-        int[] temp = fr.get(j);
-         if(temp[0]>0)
+         if(freq[j]>0)
          {
-            sb.setCharAt(i,(char) ('a' + temp[1]));
-            temp[0]--;
+            sb.setCharAt(i,(char) ('a' + j));
+            freq[j]--;
             i=i+2;
          }
          else j++;
        }
-       i = 1;
-       while(i<s.length() && j<fr.size())
+       i=1;
+        while(i<s.length() && j<26)
        {
-         int[] temp = fr.get(j);
-         if(temp[0]>0)
+         if(freq[j]>0)
          {
-            sb.setCharAt(i,(char) ('a' + temp[1]));
-            temp[0]--;
+            sb.setCharAt(i,(char) ('a' + j));
+            freq[j]--;
             i=i+2;
          }
          else j++;
        }
        return sb.toString();
     }
-}
+};
