f4a3f0288ab371649da8795cc45d8d98e86a99c4
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-11-13 16:01:40 -0500

    Time: 5 ms (79.91%) | Memory: 47 MB (6.75%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,28 +1,20 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        int len = asteroids.length;
         Stack<Integer> st = new Stack<>();
         for(int i : asteroids)
         {
-            if(i>0){
-                      st.push(i);
-            }
+            if(i>0) st.push(i);
             else{
-                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(i)){
-                    st.pop();
-                }
-                if(!st.isEmpty() && st.peek()==Math.abs(i)){
-                    st.pop();
-                }
-                else if(st.isEmpty() || st.peek()<0){
-                    //if stack is empty or its top element is less than 0
-                    st.push(i);
-                }
+                while(!st.isEmpty() && st.peek()<Math.abs(i) && st.peek()>0) st.pop();
+                if(!st.isEmpty() && st.peek()==Math.abs(i)) st.pop();
+                else if(st.isEmpty() || st.peek()<0) st.push(i);
             }
         }
         int[] ans = new int[st.size()];
-        for(int i= st.size()-1;i>-1;i--){
-            ans[i]=st.pop();
+        int j =0;
+        for(int i : st)
+        {
+          ans[j++]=i;
         }
         return ans;
     }
