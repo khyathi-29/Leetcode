2311ee13327a426013389649c1ff06f7ba4b4407
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-11-15 20:50:39 -0500

    Time: 3 ms (99.39%) | Memory: 47 MB (11.20%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,39 +1,40 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] inDegree = new int[numCourses];
-        List<Integer>[] graph = new ArrayList[numCourses];
-        int[] ans = new int[numCourses];
-        int count=0;
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-          graph[i] = new ArrayList<>();
-         }
+        int n = numCourses;
+        List<Integer>[] adj = new ArrayList[n];
+        Stack<Integer> s = new Stack<>();
+        int[] visited = new int[n];
+        int[] pathVisited = new int[n];
+        for(int i=0;i<n;i++) adj[i]= new ArrayList<>();
         for(int[] temp : prerequisites)
         {
-            int prereq = temp[1];
-            int course= temp[0];
-            inDegree[course]++;
-            graph[prereq].add(course);
+            adj[temp[1]].add(temp[0]);
         }
-        for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0){
-                q.add(i);
+        for( int i =0 ;i<n;i++){
+            if(visited[i]==0)
+            {
+                if(DFS(i,adj,s,visited,pathVisited)==false) return new int[0];
             }
         }
-        while(!q.isEmpty()){
-            int course = q.poll();
-            ans[count++]=course;
-            for(int j: graph[course]){
-                    inDegree[j]--;
-                    if(inDegree[j]==0){
-                        q.add(j);
-                    }
-                }
-        }
-       if(numCourses==count){
-        return ans;
-       }
-       else return new int[0];
-        
+        int[] ans = new int[n];
+        int j =0;
+        while(!s.isEmpty()) ans[j++]= s.pop();
+         return ans;
     }
+    public boolean DFS(int node, List<Integer>[] adj, Stack<Integer> s, int[] visited,int[] pathVisited)
+    {
+        visited[node] = 1;
+        pathVisited[node] = 1;
+        for(int i : adj[node]){
+            if(visited[i]==0) {
+                if(DFS(i,adj,s,visited,pathVisited)==false) return false;
+            }
+            else{
+                if(visited[i]==pathVisited[i]) return false;
+            }
+        }
+        pathVisited[node] =0;
+        s.push(node);
+        return true;
+        }
 }
