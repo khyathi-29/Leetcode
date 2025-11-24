bb453a7dd0a86dea9182a18c4218f8a71a479533
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-11-24 18:45:03 -0500

    Time: 159 ms (5.18%) | Memory: 54.1 MB (5.00%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-      vector<int> d(n,INT_MAX);//cost like distance matrix
-      d[src]=0;  
-      for(int i=0;i<k+1;i++){
-        vector<int> tempd = d;//we need it so that we don't take updated distances in calculating 
-        for(vector<int> details : flights){
-            int u =details[0];
-            int v = details[1];
-            int w = details[2];
-            if(d[u]!=INT_MAX && tempd[v]> d[u]+w){
-                tempd[v]=d[u]+w;
-                cout<<d[dst]<<" "<<u<<endl;
+        vector<int> distance(n,INT_MAX);
+        distance[src]=0;
+        for(int i=0;i<=k;i++){
+            vector<int> tempd = distance;
+            for(vector<int> f : flights){
+                int u = f[0];
+                int v = f[1];
+                int w = f[2];
+                if(distance[u]!=INT_MAX && tempd[v]> distance[u]+w){
+                       tempd[v] = distance[u]+w;
+                }
             }
+             distance = tempd;
         }
-        d = tempd;
-        cout<<d[dst]<<endl;
-      } 
-      if(d[dst]==INT_MAX) return -1;
-      return d[dst]; 
+        if(distance[dst]==INT_MAX) return -1;
+      return distance[dst]; 
     }
 };
