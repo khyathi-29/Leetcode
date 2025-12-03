c36d56091aa1654bb95add33e5f5596da0a96aee
khyathi-29 <141117528+khyathi-29@users.noreply.github.com>
2025-12-03 12:22:08 -0500

    Time: 12 ms (34.24%) | Memory: 14.3 MB (61.13%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         stack<int> st;
-        //vector<int> indexRemoval;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(') st.push(i);//index where bracket opens
-            else if(s[i]==')'){
-             if(st.empty()==true) s[i]='*';
-             else st.pop();
+        for(int i=0;i<s.length();i++){
+            char c = s[i];
+            if(c=='(') st.push(i);
+            else if(c==')'){
+                if(st.empty()==true) s[i]='*';
+                else st.pop();
             }
             else continue;
         }
         while(!st.empty()){
-           s[st.top()]='*';
-           st.pop(); 
-        }//make the existing open brackets without any closed as invalid;
-        string temp="";
-        for(int i=0;i<s.size();i++){
-         if(s[i]!='*') temp+=s[i];
+            int j = st.top();
+            s[j]='*';
+            st.pop();
         }
-        return temp;
+        string ans ="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]!='*'){
+                ans +=s[i];
+            }
+        }
+        return ans;
     }
 };
